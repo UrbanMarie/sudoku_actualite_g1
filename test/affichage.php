3b40079e80c8878695417177cc8437c796c97e92
Niels34410 <chaigneau.niels@gmail.com>
2017-05-20 17:53:43 +0200

    affichage 9 régions

--- test/affichage.php
+++ test/affichage.php
@@ -5,49 +5,416 @@
 	 <title>Sudoku</title>
 </head>
 <body>
+	<div id="region01">
 <?php
 require 'test_conditions.php';
 $NbrCol = 3;
 $NbrLigne = 3;
-$NbreData = count($grand_tableau);
+$NbreData = count($tableau1);
 
 if ($NbreData != 0)
 {
 	$k = 0; // indice du tableau
+	$l = 0;
+
+?>
+	<table border="1">
+	<tbody>
+			<?php	for ($i=1; $i<=$NbrLigne; $i++)
+			{ // ligne $i
+	?>		<tr style=border="1" >
+	<?php		for ($j=1; $j<=$NbrCol; $j++)
+					{ // colonne $j
+							if ($k<$NbreData) {
+	?>			<td>
+	<?php
+							print_r ($tableau1[$k]);
+
+	?>			</td>
+
+	<?php			$k++;
+							} else { // cellule vide
+	?>			<td>&nbsp;</td>
+	<?php			}
+					}
+	?>		</tr>
+	<?php	}
+	?>
+			</tbody>
+			</table>
+
+			<?php
+			} else { ?>
+					pas de données à afficher
+			<?php
+			}
+			?>
+	</div>
+
+    <div id="region02">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau2);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
     $l = 0;
 
 ?>
-    <table border="1">
-	<tbody>
+    <table border="1" style="position: absolute; top: 8px; left: 60px">
+    <tbody>
         <?php	for ($i=1; $i<=$NbrLigne; $i++)
         { // ligne $i
-    ?>		<tr>
+    ?>		<tr style=border="1" >
     <?php		for ($j=1; $j<=$NbrCol; $j++)
-    		{ // colonne $j
-    			if ($k<$NbreData) {
+            { // colonne $j
+                if ($k<$NbreData) {
     ?>			<td>
     <?php
-    			print_r ($tableau1[$k]);
+                print_r ($tableau2[$k]);
 
     ?>			</td>
 
     <?php			$k++;
-    			} else { // cellule vide
+                } else { // cellule vide
     ?>			<td>&nbsp;</td>
     <?php			}
-    		}
+            }
     ?>		</tr>
     <?php	}
     ?>
-    	</tbody>
-    	</table>
+        </tbody>
+        </table>
 
         <?php
         } else { ?>
-        	pas de données à afficher
+            pas de données à afficher
         <?php
         }
         ?>
+    </div>
 
+    <div id="region03">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau3);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1" style="position: absolute; top: 8px; left: 110px">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau3[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+
+    <div id="region04">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau4);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr border="1">
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau4[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+    <div id="region05">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau5);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1" style="position: absolute; top: 87px; left: 60px">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau5[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+    <div id="region06">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau6);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1" style="position: absolute; top: 87px; left: 110px">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau6[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+
+    <div id="region07">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau7);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau7[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+
+    <div id="region08">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau8);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1" style="position: absolute; top: 166px; left: 60px">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau8[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
+
+    <div id="region09">
+<?php
+require 'test_conditions.php';
+$NbrCol = 3;
+$NbrLigne = 3;
+$NbreData = count($tableau9);
+
+if ($NbreData != 0)
+{
+    $k = 0; // indice du tableau
+    $l = 0;
+
+?>
+    <table border="1" style="position: absolute; top: 166px; left: 110px">
+    <tbody>
+        <?php	for ($i=1; $i<=$NbrLigne; $i++)
+        { // ligne $i
+    ?>		<tr>
+    <?php		for ($j=1; $j<=$NbrCol; $j++)
+            { // colonne $j
+                if ($k<$NbreData) {
+    ?>			<td>
+    <?php
+                print_r ($tableau9[$k]);
+
+    ?>			</td>
+
+    <?php			$k++;
+                } else { // cellule vide
+    ?>			<td>&nbsp;</td>
+    <?php			}
+            }
+    ?>		</tr>
+    <?php	}
+    ?>
+        </tbody>
+        </table>
+
+        <?php
+        } else { ?>
+            pas de données à afficher
+        <?php
+        }
+        ?>
+    </div>
         </body>
         </html>
